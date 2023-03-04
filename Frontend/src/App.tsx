dbe11385324502b651bc105f3a019f58662446fe
bheemaguli <sumanth.bhi@gmail.com>
2023-03-04 18:04:08 +0530

    Create backend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,68 @@
+import React, { Fragment, useState } from 'react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import { TopGradient, BottomGradient } from './layouts/BackGround/BackGround'
+import { NavBar } from './layouts/NavBar/NavBar'
+import { Card } from 'flowbite-react'
+
+const products = [
+  {
+    name: 'Analytics',
+    description: 'Get a better understanding of your traffic',
+    href: '#',
+    icon: ChartPieIcon,
+  },
+  {
+    name: 'Engagement',
+    description: 'Speak directly to your customers',
+    href: '#',
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: 'Security',
+    description: 'Your customers’ data will be safe and secure',
+    href: '#',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Integrations',
+    description: 'Connect with third-party tools',
+    href: '#',
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: 'Automations',
+    description: 'Build strategic funnels that will convert',
+    href: '#',
+    icon: ArrowPathIcon,
+  },
+]
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <React.Fragment>
+      <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-screen h-screen'>
+        <NavBar />
+        <Card>Hello</Card>
+      </div>
+    </React.Fragment>
+  )
+}
--- Frontend/src/layouts/NavBar/NavBar.tsx
+++ Frontend/src/layouts/NavBar/NavBar.tsx
@@ -56,9 +56,9 @@ export function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div>
+    <div className='bg-white opacity-80 backdrop-blur'>
       <nav
-        className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
+        className='mx-auto flex w-100 items-center justify-between p-6 lg:px-8 '
         aria-label='Global'
       >
         <div className='flex lg:flex-1'>
@@ -154,7 +154,7 @@ export function NavBar() {
       </nav>
       <Dialog as='div' className='lg:hidden' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className='fixed inset-0 z-10' />
-        <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+        <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 iy-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
             <a href='#' className='-m-1.5 p-1.5'>
               <span className='sr-only'>Your Company</span>
